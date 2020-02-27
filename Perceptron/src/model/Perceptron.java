de0a9c0deb8bf5a1e8d6149bfabbd2c38e07a071
haseokwoo <seokwooha@student.cofo.edu>
2020-02-27 11:26:04 -0600

    ddd

--- Perceptron/src/model/Perceptron.java
+++ Perceptron/src/model/Perceptron.java
@@ -4,17 +4,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Perceptron {
+	public static final Double DEFAULT_WEIGHT = 0.1;
 	private List<Boolean> input = new ArrayList<>();
 	private List<Double> inputWeights = new ArrayList<>();
 	private double threshold = 1;
 	private double velocity = .2;
 	
+	public List<Boolean> getInput() {
+		return input;
+	}
+
+	public List<Double> getInputWeights() {
+		return inputWeights;
+	}
+
+	public double getThreshold() {
+		return threshold;
+	}
+
+	public double getVelocity() {
+		return velocity;
+	}
+
 	public boolean isFiring() {
 		int runningWeights = 0;
 		for (int n=0; n < input.size(); ++n) {
 			runningWeights += (input.get(n) ? 1 : 0) * inputWeights.get(n);
 		}
 		return runningWeights >= threshold;
+	}
+
+	public void setInputWeights(List<Double> inputWeights) {
+		this.inputWeights = inputWeights;
+	}
+
+	public void setThreshold(double threshold) {
+		this.threshold = threshold;
+	}
+
+	public void setVelocity(double velocity) {
+		this.velocity = velocity;
+	}
+	
+	public void addInput(boolean value) {
+		input.add(value);
+		inputWeights.add(DEFAULT_WEIGHT);
+	}
+	
+	public void addInput(boolean value, double weight) {
+		input.add(value);
+		inputWeights.add(weight);
+	}
+	
+	public void setInput(int index, boolean value) {
+		input.set(index, value);
 	}
 	
 }
--- Perceptron/src/ui/Main.java
+++ Perceptron/src/ui/Main.java
@@ -1,7 +1,31 @@
 package ui;
 
+import java.util.Arrays;
+
+import model.Perceptron;
+
 public class Main {
 	public static void main(String[] args) {
+		Perceptron p = new Perceptron();
 		
+		//logical and false,false...
+		p.addInput(false);
+		p.addInput(false);
+		System.out.println(p.isFiring());
+		
+		//logical and false,true...
+		p.setInput(0,false);
+		p.setInput(1,true);
+		System.out.println(p.isFiring());
+		
+		//logical and true,false...
+		p.setInput(0,true);
+		p.setInput(1,false);
+		System.out.println(p.isFiring());
+		
+		//logical and true,false...
+		p.setInput(0,true);
+		p.setInput(1,true);
+		System.out.println(p.isFiring());
 	}
 }
